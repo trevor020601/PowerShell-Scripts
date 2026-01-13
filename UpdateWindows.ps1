d96db1eb2276ea09e1dbfb7d866ca24aae0b004f
trevor020601 <trevorkarl5@gmail.com>
2026-01-12 20:13:22 -0600

    Bump up to .NET 10 and script to automatically update Windows; Consider scrapping this repository or outright redoing it

---
+++ UpdateWindows.ps1
@@ -0,0 +1,9 @@
+﻿# Needs to be ran with Administrator privileges
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process -Force
+
+Install-Module -Name PSWindowsUpdate -Force -SkipPublisherCheck
+
+Import-Module PSWindowsUpdate -Force
+
+Get-WindowsUpdate -MicrosoftUpdate -AcceptAll -Install -AutoReboot -Verbose
