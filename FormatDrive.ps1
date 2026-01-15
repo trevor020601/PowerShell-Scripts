fcf7c681e50545e274085dcb0a6adae17bd1b1c3
trevor020601 <trevorkarl5@gmail.com>
2026-01-14 20:41:26 -0600

    Script to format a drive

---
+++ FormatDrive.ps1
@@ -0,0 +1,11 @@
+﻿param(
+	[Parameter(Mandatory=$true)]
+	[string]$DriveLetter,
+	[Parameter(Mandatory=$true)]
+	[ValidateSet("NTFS","FAT32","exFAT","ReFS")]
+	[string]$FileSystem,
+	[Parameter(Mandatory=$true)]
+	[string]$NewDriveName
+)
+
+Format-Volume -DriveLetter $DriveLetter -FileSystem $FileSystem -NewFileSystemLabel $NewDriveName
