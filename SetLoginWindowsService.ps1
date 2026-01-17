20d6161397a72647ec3c53f64ae8f8837f08a42c
trevor020601 <trevorkarl5@gmail.com>
2026-01-16 21:06:43 -0600

    Script to set login for windows service

---
+++ SetLoginWindowsService.ps1
@@ -0,0 +1,20 @@
+﻿#Requires -RunAsAdministrator
+
+using namespace System.Management.Automation
+
+param(
+	[Parameter(Mandatory=$true)]
+	[string]$ServiceName,
+	[Parameter(Mandatory=$true)]
+	[string] $UserName,
+	[Parameter(Mandatory=$true)]
+	[string] $Password
+)
+
+$SecurePassword = ConvertTo-SecureString $Password -AsPlainText -Force
+
+$Credential = New-Object PSCredential ($UserName, $SecurePassword)
+
+Set-Service -Name $ServiceName -Status Stopped
+Set-Service -Name $ServiceName -Credential $Credential
+Set-Service -Name $ServiceName -Status Running
