12214275d2c74794c351e5c7fecbbd22cab2450b
trevor020601 <trevorkarl5@gmail.com>
2026-01-17 13:32:27 -0600

    Script to copy file to a destination

---
+++ CopyFile.ps1
@@ -0,0 +1,12 @@
+﻿param(
+	[Parameter(Mandatory=$true)]
+	[string]$SourceFile,
+	[Parameter(Mandatory=$true)]
+	[string] $DestinationDirectory
+)
+
+if (-not (Test-Path -Path $DestinationDirectory)) {
+	New-Item -ItemType Directory -Path $DestinationDirectory -Force
+}
+
+Copy-Item -Path $SourceFile -Destination $DestinationDirectory -Force
