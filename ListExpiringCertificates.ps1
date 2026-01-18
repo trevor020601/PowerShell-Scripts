0a596328acbdca8a940ffe7628d55babee92608a
trevor020601 <trevorkarl5@gmail.com>
2026-01-18 16:33:02 -0600

    Script to list expiring certificates

---
+++ ListExpiringCertificates.ps1
@@ -0,0 +1,15 @@
+﻿param(
+    [Parameter(Mandatory=$true)]
+    [string] $CertificatePath,
+	[Parameter(Mandatory=$true)]
+	[int] $DaysToExpiration
+)
+
+$ExpirationDate = (Get-Date).AddDays($DaysToExpiration)
+
+Get-ChildItem -Path $CertificatePath -Recurse | 
+    Where-Object { $_.NotAfter -lt $ExpirationDate } | 
+    Select-Object Subject, Issuer, NotAfter, FriendlyName, @{
+        Label = "Expires In (Days)"; 
+        Expression = { ($_.NotAfter - (Get-Date)).Days }
+    } | Format-Table -AutoSize
